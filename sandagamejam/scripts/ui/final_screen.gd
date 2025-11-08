28fd36cb70fcd3c69b1fad59eead94b0ebf58e6c
Melissa <melissahuerta.dev@gmail.com>
2025-11-07 21:29:27 -0500

    feat: listar los ultimos 10, dejar que el player se agregue si alcanzo el ranking

--- sandagamejam/scripts/ui/final_screen.gd
+++ sandagamejam/scripts/ui/final_screen.gd
@@ -14,6 +14,15 @@ extends Control
 @onready var ranking_container = $RankingContainer
 @onready var play_again_btn = $BtnPlayAgain
 @onready var recipe_texture = $Recipe
+@onready var loading_label = $LoadingLabel
+
+# Loading text animation
+@export var dot_speed: float = 0.5 # Time in seconds to add/remove a dot
+@export var max_dots: int = 3
+
+var current_dots: int = 0
+var base_text: String = "Loading" # TO DO REPLACE BY LANGUAGE
+var timer: Timer
 
 # Preloads de texturas
 @onready var bg_win   = preload("res://assets/backgrounds/good_score_bg.png")
@@ -24,10 +33,12 @@ extends Control
 @onready var recipe_win = preload("res://assets/pastry/recipes/recipe_003.png")
 @onready var ranking_label_settings: LabelSettings = preload("res://custom_resources/Ranking.tres")
 
+var add_new_score: bool = false
 var name_entered: bool = false
 var score: int = 100
 var max_name_length: int = 6
 var current_name: Array = []
+var cached_entries: Array = []
 var ranking: Array = []
 var menu_labels = GlobalManager.menu_labels[GlobalManager.game_language]
 var settings_instance = preload("res://custom_resources/Ranking.tres").duplicate()
@@ -43,10 +54,11 @@ func _ready():
 	message_label.label_settings = settings_instance
 
 	score_label.text = menu_labels["ranking"]["score"] + " " + str(score)
+	add_new_score = await is_player_in_ranking(score)
 	show_name_label()
 	
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed:
+	if event is InputEventKey and event.pressed and add_new_score:
 		#a-z y A-Z
 		if (event.unicode >= 65 and event.unicode <=90) or (event.unicode >= 97 and event.unicode <= 122):
 			var char_typed = char(event.unicode).to_upper()
@@ -59,8 +71,9 @@ func _input(event: InputEvent) -> void:
 				show_name_label()
 			# Borrar letra
 		elif event.keycode == KEY_ENTER and current_name.size() > 0 and not name_entered:
-			load_entries_from_talo()
-			store_in_talo("".join(current_name), score)
+			hide_player_score_labels()
+			animate_loading_label()
+			await store_in_talo("".join(current_name), score)
 			show_ranking()
 			name_entered = true
 	
@@ -92,6 +105,11 @@ func show_final_screen(state: GlobalManager.GameState):
 	
 func show_name_label():
 	var display = ""
+	
+	if not add_new_score:
+		name_label.text = display
+		return
+	
 	for i in range(max_name_length):
 		if i < current_name.size():
 			display += current_name[i] + ""
@@ -99,11 +117,25 @@ func show_name_label():
 			display += "_ "
 	name_label.text =  menu_labels["ranking"]["name"] + "\n" + display 
 	
+# Animar el "Loading..."
+func animate_loading_label():
+	loading_label.visible = true # TO DO ANIMATE LOADING LABEL
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = dot_speed
+	timer.autostart = true
+	timer.timeout.connect(update_dots)
+	update_dots()
+
+func update_dots():
+	current_dots = (current_dots + 1) % (max_dots + 1)
+	loading_label.text = base_text + ".".repeat(current_dots)
+	
 # Talo Calls
 func store_in_talo(username: String, score_value: int) -> void:
-	print("to store in talo... ", username, " ", score_value)
 	await Talo.players.identify("username", username)
-	var res := await Talo.leaderboards.add_entry(leaderboard_internal_name, score_value)
+	#var res :=
+	await Talo.leaderboards.add_entry(leaderboard_internal_name, score_value)
 	_build_entries()
 
 func load_entries_from_talo() -> void:
@@ -114,8 +146,11 @@ func load_entries_from_talo() -> void:
 		var options := Talo.leaderboards.GetEntriesOptions.new()
 		options.page = page
 		var res := await Talo.leaderboards.get_entries(leaderboard_internal_name, options)
+
+		# show entries!
 		var entries: Array[TaloLeaderboardEntry] = res.entries
-		var count: int = res.count
+		
+		#var count: int = res.count
 		var is_last_page: bool = res.is_last_page
 		if is_last_page:
 			done = true
@@ -124,10 +159,17 @@ func load_entries_from_talo() -> void:
 	
 	_build_entries()
 
+# Evaluar si el score entra al ranking
+func is_player_in_ranking(score: int) -> bool:
+	await load_entries_from_talo()
+	var last_child_idx = cached_entries.size()
+	var lower_score =  cached_entries[last_child_idx-1].score
+	
+	return score > lower_score
+	
 # Muestra el ranking
 func show_ranking():
-	message.visible = false
-	score_container.visible = false
+	loading_label.visible = false
 	
 	var label = play_again_btn.get_node("Label")
 	label.text = menu_labels["play_again"] 
@@ -149,8 +191,11 @@ func _build_entries() -> void:
 	free_container_children()
 	
 	# Get cached entries, recently stored
-	var cached_entry = Talo.leaderboards.get_cached_entries(leaderboard_internal_name)
-	for entry in cached_entry:
+	cached_entries = Talo.leaderboards.get_cached_entries(leaderboard_internal_name)
+	if cached_entries.size() > 10:
+		cached_entries = cached_entries.slice(0, 10)
+	
+	for entry in cached_entries:
 		_create_entry(entry)
 	
 func _create_entry(entry: TaloLeaderboardEntry) -> void:
@@ -165,11 +210,14 @@ func free_container_children():
 	for child in ranking_container.get_children():
 		child.queue_free() 
 
-# OLD FUNCTIONS
+func hide_player_score_labels():
+	message.visible = false
+	score_container.visible = false
+	
 # Replaced by Talo
-func store_in_ranking(username: String, score_value: int):
-	ranking.append({"name": username, "score": score_value})
-	# Ordenar de mayor a menor score
-	ranking.sort_custom(func(a, b): return b.score - a.score)
-	if ranking.size() > 10:
-		ranking = ranking.slice(0, 10)
+#func store_in_ranking(username: String, score_value: int):
+	#ranking.append({"name": username, "score": score_value})
+	## Ordenar de mayor a menor score
+	#ranking.sort_custom(func(a, b): return b.score - a.score)
+	#if ranking.size() > 10:
+		#ranking = ranking.slice(0, 10)
